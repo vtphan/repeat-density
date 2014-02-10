7bd9f74d43753c59e5efa4a78ed5637cfe055090
Vinhthuy Phan <vphan@memphis.edu>
2014-02-10 13:17:47 -0600

    print out genome file name as id

--- genome_complexity.go
+++ genome_complexity.go
@@ -137,7 +137,8 @@ func main(){
     idx.build(os.Args[1])
     // fmt.Println(idx.sa, idx.lcp)
     // fmt.Println(idx.D(), idx.Dk(2), idx.Rk(2))
-    fmt.Printf("I\tD\tD_100\tD_200\tD_400\tR_100\tR_200\tR_400\n")
-    fmt.Printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", idx.I(), idx.D(),
+    fmt.Printf("Genome\tI\tD\tD_100\tD_200\tD_400\tR_100\tR_200\tR_400\n")
+    fmt.Printf("%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",
+        os.Args[1], idx.I(), idx.D(),
         idx.Dk(100), idx.Dk(200), idx.Dk(400), idx.Rk(100), idx.Rk(200), idx.Rk(400))
 }
